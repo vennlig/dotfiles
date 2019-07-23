ac3b91c7eccf3cddfd3d2cd3b29958cafc878f1c
song <songpokie@gmail.com>
2019-07-23 06:22:03 +0200

    woops

---
+++ config/.config/polybar/scripts/wal-colors.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PDIR="$HOME/.config/polybar"
+LAUNCH="polybar-msg cmd restart"
+
+sed -i -e 's/bg = .*/bg = ${xrdb:color0:#222}/g' $PDIR/colors.ini
+sed -i -e 's/fg = .*/fg = ${xrdb:color7:#222}/g' $PDIR/colors.ini
+sed -i -e 's/ac = .*/ac = ${xrdb:color1:#222}/g' $PDIR/colors.ini
+sed -i -e 's/bi = .*/bi = ${xrdb:color1:#222}/g' $PDIR/colors.ini
+sed -i -e 's/be = .*/be = ${xrdb:color1:#222}/g' $PDIR/colors.ini
+
+# Color of menu button thingy
+sed -i -e 's/mf = .*/mf = ${xrdb:color0:#222}/g' $PDIR/colors.ini
+
+$LAUNCH &
