5b4fbd9c26c2b0aed6f557014d5a14f47a357103
song <songpokie@gmail.com>
2019-07-24 03:31:19 +0200

    redshift launch script

---
+++ redshift/.redshift/launch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# Terminate already running redshift instances
+killall -q redshift 
+
+# Wait for processes to shut down
+while pgrep -u $UID -x redshift >/dev/null; do sleep 1; done
+
+# Launch redshift
+redshift -l  60.78333:10.63333 &
