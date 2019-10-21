0a6de116e352098b81aebb26bb1a62116d8cac50
song <songpokie@gmail.com>
2019-10-21 12:30:54 +0200

    emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -3,12 +3,33 @@
          '("melpa" . "http://melpa.org/packages/") t)
 
 (package-initialize)
+
+(require 'evil)
+(evil-mode 1)
+
+(add-hook 'after-init-hook 'global-company-mode)
+
+(require 'eglot)
+(add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
+(add-hook 'c-mode-hook 'eglot-ensure)
+(add-hook 'c++-mode-hook 'eglot-ensure)
+
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
+
+(require 'rust-mode)
+(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(setq company-tooltip-align-annotations t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (## rust-mode evil-visual-mark-mode))))
+ '(package-selected-packages
+   (quote
+    (eglot company racer evil ## rust-mode evil-visual-mark-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
