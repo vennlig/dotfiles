6e4f144f4321980f4e247c24343e777f5546a21d
song <songpokie@gmail.com>
2019-10-21 16:07:36 +0200

    Changed theme for emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -32,6 +32,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
+ '(custom-enabled-themes (quote (manoj-dark)))
  '(package-selected-packages
    (quote
     (org eglot company racer evil ## rust-mode evil-visual-mark-mode))))
