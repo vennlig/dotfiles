704bdf7e36c0732299759c90c4c93fc0362a825c
Slowpokie <songpokie@gmail.com>
2019-10-25 14:41:34 +0200

    emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -5,48 +5,111 @@
 (package-initialize)
 (package-refresh-contents)
 
-(setq my-package-list '(company evil rtags all-the-icons neotree solaire-mode rust-mode cargo racer))
+;; my package list
+(setq my-package-list '(company helm  helm-gtags evil rtags all-the-icons neotree centaur-tabs solaire-mode rust-mode cargo racer))
 (mapc #'package-install my-package-list)
 
-
+;; all the icons
 (require 'all-the-icons)
+
+;; neotree
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 
-(require 'solaire-mode)
+;; centaur tabs
+(require 'centaur-tabs)
+(centaur-tabs-mode t)
+(global-set-key (kbd "C-<prior>") 'centaur-tabs-backward)
+(global-set-key (kbd "C-<next>") 'centaur-tabs-forward)
 
-(solaire-global-mode +1)
-
-(add-hook 'ediff-prepare-buffer-hook #'solaire-mode)
-(add-hook 'after-revert-hook #'turn-on-solaire-mode)
-(add-hook 'minibuffer-setup-hook #'solaire-mode-in-minibuffer)
-
-(solaire-mode-swap-bg)
+(setq centaur-tabs-style "bar")
+(setq centaur-tabs-set-bar 'over)
+(setq centaur-tabs-gray-out-icons 'buffer)
+(setq centaur-tabs-set-icons t)
 
 
+;; solaire
+;; (require 'solaire-mode)
+
+;; (solaire-global-mode +1)
+
+;; (add-hook 'ediff-prepare-buffer-hook #'solaire-mode)
+;; (add-hook 'after-revert-hook #'turn-on-solaire-mode)
+;; (add-hook 'minibuffer-setup-hook #'solaire-mode-in-minibuffer)
+
+;; (solaire-mode-swap-bg)
+
+;; org
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
 
+
+;; helm
+(require 'helm)
+(require 'helm-config)
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+
+
+;; https://tuhdo.github.io/helm-intro.html 
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-z") 'helm-select-action)
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p       t
+      helm-move-to-line-cycle-in-source t
+      helm-ff-search-library-in-sexp    t
+      helm-scroll-amount                8
+      helm-ff-file-name-history-use-recentf t
+      helm-echo-input-in-header-line    t)
+      
+
+(defun spacemacs//helm-hide-minibuffer-maybe ()
+  (when (with-helm-buffer helm-echo-input-in-header-line)
+    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+      (overlay-put ov 'window (selected-window))
+      (overlay-put ov 'face
+                   (let ((bg-color (face-background 'default nil)))
+                     `(:background ,bg-color :foreground ,bg-color)))
+      (setq-local cursor-type nil))))
+
+
+(add-hook 'helm-minibuffer-set-up-hook
+          'spacemacs//helm-hide-minibuffer-maybe)
+
+(setq helm-autoresize-max-height 0)
+(setq helm-autoresize-min-height 20)
+(helm-autoresize-mode 1)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+
+(helm-mode 1)
+
+;; evil
 (require 'evil)
 (evil-mode 1)
 
-
-(add-hook 'after-init-hook 'global-company-mode)
+;; company
+;; (add-hook 'after-init-hook 'global-company-mode)
 
 ;; C/C++
 (add-hook 'c-mode-hook 'rtags-start-process-unless-running)
 (add-hook 'c++-mode-hook 'rtags-start-process-unless-running)
 
-
 ;; rust
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'racer-mode-hook #'eldoc-mode)
-(add-hook 'racer-mode-hook #'company-mode)
+;; (add-hook 'racer-mode-hook #'company-mode)
 
 (require 'rust-mode)
-(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+;; (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
 (setq company-tooltip-align-annotations t)
 
 (add-hook 'rust-mode-hook 'cargo-minor-mode)
@@ -63,10 +126,12 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
- '(custom-enabled-themes nil)
+ '(custom-enabled-themes (quote (manoj-dark)))
+ '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (all-the-icons rtags org eglot company racer evil ## rust-mode evil-visual-mark-mode))))
+    (helm centaur-tabs all-the-icons rtags org eglot company racer evil ## rust-mode evil-visual-mark-mode)))
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
