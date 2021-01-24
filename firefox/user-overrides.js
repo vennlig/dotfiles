1967b6e33b1a48d3c600b30f7f1e2a36f5cf61fd
vennlig <songpokie@gmail.com>
2021-01-24 02:04:42 +0100

    ..

--- firefox/user-overrides.js
+++ firefox/user-overrides.js
@@ -1,11 +1,5 @@
+/* Send downloads to google :) */
 user_pref("browser.safebrowsing.downloads.remote.enabled", true);
-
-/* Media permissions set to block by default */
-// user_pref("permissions.default.camera", 2);
-// user_pref("permissions.default.microphone", 2);
-// user_pref("permissions.default.desktop-notification", 2);
-// user_pref("permissions.default.geo", 2);
-// user_pref("permissions.default.shortcuts", 2);
 
 user_pref("media.autoplay.default", 5);
 // user_pref("full-screen-api.enabled", false);
@@ -22,19 +16,15 @@ user_pref("dom.event.clipboardevents.enabled", false);
 user_pref("dom.webnotifications.enabled", false);
 
 /* Ion and baseline JIT */
-/*
-* user_pref("javascript.options.ion", false);
-* user_pref("javascript.options.baselinejit", false);
-* user_pref("javascript.options.jit_trustedprincipals", true);
-*/
-
+// user_pref("javascript.options.ion", false);
+// user_pref("javascript.options.baselinejit", false);
+// user_pref("javascript.options.jit_trustedprincipals", true);
 
 /* Disable battery metric */
 user_pref("dom.battery.enabled", false);
 
 /* disable virtual reality devices */
 user_pref("dom.vr.enabled", false);
-// user_pref("permissions.default.xr", 2);
 
 /* disable mathml (mathematical markup language) */
 user_pref("mathml.disabled", true);
@@ -48,30 +38,13 @@ user_pref("browser.download.forbid_open_with", true);
 /* disable service worker cache and cache storage */
 user_pref("dom.caches.enabled", false);
 
-
-/* fingerprinting */
-
-/* disable device sensor API */
-// user_pref("device.sensors.enabled", false);
-
-/* disable gamepad api */
-// user_pref("dom.gamepad.enabled", false);
-
-/* disable speechsynthesis */
-// user_pref("media.webspeech.synth.enabled", false);
-
-/* session restore */
-user_pref("browser.sessionstore.max_tabs_undo", 0);
-
 /* disable resuming from crash */
 user_pref("browser.sessionstore.resume_from_crash", false);
 
 /* disable uploading screenshots to screenshot server */
 user_pref("extensions.screenshots.upload-disabled", true);
 
-
-/* search */
-/* disable suggestions */
+/* disable search suggestions */
 user_pref("browser.urlbar.suggest.history", false);
 user_pref("browser.urlbar.suggest.bookmark", true);
 user_pref("browser.urlbar.suggest.openpage", true);
@@ -83,7 +56,6 @@ user_pref("browser.urlbar.autoFill", false);
 /* disable saving passwords */
 user_pref("signon.rememberSignons", false);
 
-
 /* don't allow sub-resources to open HTTP authentication credentials dialogs */
 user_pref("network.auth.subresource-http-auth-allow", 0);
 
@@ -94,9 +66,7 @@ user_pref("media.media-capabilities.enabled", false);
 /* English */
 user_pref("privacy.spoof_english", 2);
 
-
 user_pref("privacy.resistFingerprinting.letterboxing", false);
-
 
 /* extension stuff */
 /* pocket; idk if this does anything */
@@ -110,5 +80,10 @@ user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", 
 user_pref("browser.chrome.site_icons", false); // 1031 // no favicons in history/bookmarks
 user_pref("alerts.showFavicons", false); // 1032 // no favicons in web notifications
 
-/* 2654: disable "open with" in download dialog [FF50+] [SETUP-HARDEN] */
-user_pref("browser.download.forbid_open_with", true);
+/* media permissions set to block by default */
+// user_pref("permissions.default.camera", 2);
+// user_pref("permissions.default.microphone", 2);
+// user_pref("permissions.default.desktop-notification", 2);
+// user_pref("permissions.default.geo", 2);
+// user_pref("permissions.default.shortcuts", 2);
+// user_pref("permissions.default.xr", 2);
